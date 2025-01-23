10ba916213af345e9d00e921eb41a7b35ac7b9d4
kararei <kararei@gmail.com>
2025-01-24 02:30:36 +0800

    Recreated to kx with greeting

---
+++ src/main/java/kx.java
@@ -0,0 +1,10 @@
+public class kx {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm kx, the always kai xin bot!\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye! I hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
