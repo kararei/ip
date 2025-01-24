f3b758b8607e4796ba97879fc3114cc736258052
kararei <kararei@gmail.com>
2025-01-24 12:00:39 +0800

    Add echo ability

--- src/main/java/kx.java
+++ src/main/java/kx.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class kx {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm kx, the always kai xin bot!\nWhat can I do for you?");
+        System.out.println("  Hello! I'm kx, the kai xin bot!\n  What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye! I hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("  Bye bye and hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println("  " + input);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
 
     }
 }
