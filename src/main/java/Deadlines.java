48e98685174ba3ade6ac8413df22796b7dbfaf36
kararei <kararei@gmail.com>
2025-02-11 23:15:39 +0800

    Add DateTime formatting ability

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,10 +1,22 @@
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadlines extends Task{
 
-    protected String by;
+    protected LocalDateTime by;
+    private static final DateTimeFormatter input_formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
+    private static final DateTimeFormatter output_formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    private static final DateTimeFormatter file_formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
 
     public Deadlines(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDateTime.parse(by, input_formatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("Error in Input format: Input should follow dd-MM-yyyy HHmm format ");
+        }
     }
 
     @Override
@@ -14,12 +26,12 @@ public class Deadlines extends Task{
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(output_formatter) + ")";
     }
 
     @Override
     public String toFileFormat() {
-        String output = String.format("D | %d | %s | %s\n", isDone ? 1 : 0, description, by);
+        String output = String.format("D | %d | %s | %s\n", isDone ? 1 : 0, description, by.format(input_formatter));
         return output;
     }
 }
--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,12 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Events extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private static final DateTimeFormatter input_formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
+    private static final DateTimeFormatter output_formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    private static final DateTimeFormatter file_formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
 
     public Events(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+
+        try {
+            this.from = LocalDateTime.parse(from, input_formatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("Error in Input format: Input should follow dd-MM-yyyy HHmm format ");
+        }
+
+        try {
+            this.to = LocalDateTime.parse(to, input_formatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("Error in Input format: Input should follow dd-MM-yyyy HHmm format ");
+        }
     }
 
     public Events(String description, String timing) {
@@ -21,12 +38,12 @@ public class Events extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(output_formatter) + " to: " + to.format(output_formatter) + ")";
     }
 
     @Override
     public String toFileFormat() {
-        String output = String.format("E | %d | %s | %s-%s\n", isDone ? 1 : 0, description, from, to);
+        String output = String.format("E | %d | %s | %s | %s\n", isDone ? 1 : 0, description, from.format(input_formatter), to.format(input_formatter));
         return output;
     }
 }
